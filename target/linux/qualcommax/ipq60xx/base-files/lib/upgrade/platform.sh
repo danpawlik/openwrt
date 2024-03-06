d2e76b3dfabe58edf7256e97e230a3fd9b9c48a6
JiaY-shi <shi05275@163.com>
2024-03-06 16:07:40 +0800

    QualcommAX: ipq60xx: add support for JD Cloud AX1800 Pro

--- target/linux/qualcommax/ipq60xx/base-files/lib/upgrade/platform.sh
+++ target/linux/qualcommax/ipq60xx/base-files/lib/upgrade/platform.sh
@@ -130,6 +130,11 @@ platform_do_upgrade() {
 	qihoo,360v6)
 		nand_do_upgrade "$1"
 		;;
+	jdc,ax1800-pro)
+		kernelname="0:HLOS"
+		rootfsname="rootfs"
+		mmc_do_upgrade "$1"
+		;;
 	tplink,eap610-outdoor)
 		tplink_do_upgrade "$1"
 		;;
