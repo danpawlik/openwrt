14aaeed55324744efb0d91636a198c3a4a7159ab
Daniel Pawlik <pawlik.dan@gmail.com>
2026-03-06 08:44:57 +0100

    Add uboot env tools

--- target/linux/airoha/an7581/target.mk
+++ target/linux/airoha/an7581/target.mk
@@ -7,6 +7,7 @@ FEATURES+=pwm source-only
 
 DEFAULT_PACKAGES += \
 	airoha-en7581-npu-firmware uboot-envtools
+DEPENDS+=+TARGET_airoha_an7581
 
 define Target/Description
 	Build firmware images for Airoha an7581 ARM based boards.
